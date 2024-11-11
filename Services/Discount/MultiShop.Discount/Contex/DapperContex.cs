bc8f1a4ef782cf31616dbb69124ff3e5c77486aa
betulunver <betulunver@outlook.com>
2024-11-12 01:28:01 +0300

    discount service added.

---
+++ Services/Discount/MultiShop.Discount/Contex/DapperContex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+namespace MultiShop.Discount.Contex
+{
+    public class DapperContex : DbContext
+    {
+        private readonly   IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContex(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection"); //appsettingsteki isim
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = LAPTOP-D5489TA1; Initial Catalog=MultiShopDiscountDb; Integrated Security = True;");
+
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
